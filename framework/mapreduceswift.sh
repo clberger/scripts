e222615a5fd187e94ab2fe3017dd66a14bb5d4e1
Matt Lee <mrlee@uchicago.edu>
2012-05-31 14:19:12 -0500

    whoops

--- framework/mapreduceswift.sh
+++ framework/mapreduceswift.sh
@@ -9,17 +9,11 @@ while [ $# -gt 0 ]; do
     -reducefn)        reducefn=$2;;
     -combinefn)       combinefn=$2;;
     -shellscr)        shellscr=$2;;
-    -nnodes)          nnodes=$2;;
     *) echo "$0: bad args" 1>&2
        exit 1;;
   esac
   shift 2
 done
 
-for i in `seq 0 ${nnodes}`
-do
-  new_red_fn=`echo ${reducefn} | awk '{print $1 $i ".py"}'`
-  cp ${reducefn} ${new_red_fn}
-done
-swift ${script} -reducefn ${reducefn}
+swift ${script}
 python ${aggregate} ${results}
